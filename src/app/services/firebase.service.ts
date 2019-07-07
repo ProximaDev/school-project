f06817071c181b35b539c4d6f9f370e40aca0ff4
Rami Saad <ra997mi@gmail.com>
2019-07-08 00:31:56 +0300

    Remove useless funs

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, DocumentData } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { formatDate } from '@angular/common';
 import * as firebase from 'firebase/app'
 import { AngularFireDatabase } from '@angular/fire/database';
 
@@ -56,117 +55,7 @@ export class FirebaseService {
     return this.db.list(`${colName}`).valueChanges();
   }
 
-  addEvent(title, content) {
-    let today = new Date();
-    let date = formatDate(today, 'medium', 'en-US');
-    const id = this.firestore.createId();
-    this.firestore.doc(`eventList/${id}`).set({
-      id,
-      title,
-      content,
-      date
-    });
-  }
-
-  getEvents() {
-    return this.firestore.collection('eventList').valueChanges();
-  }
-
-  deleteEvent(id) {
-    this.firestore.doc(`eventList/${id}`).delete();
-  }
-
-  addDegree(name, selection, stage, subject, type, degrees) {
-    const id = this.firestore.createId();
-    this.firestore.doc(`Degree/${id}`).set({
-      id,
-      name,
-      selection,
-      stage,
-      subject,
-      type,
-      degrees,
-    });
-  }
-
-  updateDegree(id, name, selection, stage, subject, type, degrees) {
-    this.firestore.doc(`Degree/${id}`).set({
-      id,
-      name,
-      selection,
-      stage,
-      subject,
-      type,
-      degrees,
-    });
-  }
-  getDegree() {
-    return this.firestore.collection('Degree').valueChanges();
-  }
-
-
-  addAbsent(name, selection, stage, subject, adate) {
-    const id = this.firestore.createId();
-    this.firestore.doc(`Absents/${id}`).set({
-      id,
-      name,
-      selection,
-      stage,
-      subject,
-      adate
-    });
-  }
-
-  updateAbsent(id, name, selection, stage, subject, adate) {
-    this.firestore.doc(`Absents/${id}`).set({
-      id,
-      name,
-      selection,
-      stage,
-      subject,
-      adate
-    });
-  }
-  getAbsent() {
-    return this.firestore.collection('Absents').valueChanges();
-  }
-
-  addweekly(stage, selection, image, imgname) {
-    let filter = stage + "_" + selection;
-    const id = this.firestore.createId();
-    this.firestore.doc(`weeklyList/${id}`).set({
-      id,
-      stage,
-      selection,
-      image,
-      imgname,
-      filter
-    });
-  }
-
-  getWeekly() {
-    return this.firestore.collection('weeklyList').valueChanges();
-  }
-
-  deleteWeekly(id, imgid) {
-    const storageRef = firebase.storage().ref();
-    storageRef.child(`weekly/${imgid}`).delete();
-    this.firestore.doc(`weeklyList/${id}`).delete();
-  }
-
-  getStudents() {
-    return this.firestore.collection('Students').valueChanges();
-  }
-
-  getCourse(stage: string) {
-    return this.firestore.doc(`Courses/${stage}`).valueChanges();
-  }
-
-  getClass(stage: string) {
-    return this.firestore.doc(`Classes/${stage}`).valueChanges();
-  }
-
-  async login(email, password) {
+  async login(email: string, password: string) {
     return await this.afAuth.auth.signInWithEmailAndPassword(email, password);
   }
 }
