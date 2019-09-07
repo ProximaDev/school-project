117c5b6b0b6b9815f5349156f440fcbf20545647
Rami Saad <ra997mi@gmail.com>
2019-09-07 16:58:06 +0300

    chat theme

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
-import { AngularFireList, AngularFireDatabase } from '@angular/fire/database';//مكتبات جاهزة
+import { AngularFireList, AngularFireDatabase } from '@angular/fire/database';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
@@ -10,44 +10,26 @@ export class ChatComponent implements OnInit {
   allmessages = [];
   allkeys = [];
   summaries = [];
-  list=[];
+  list = [];
   newmessage;
   selectedValue: any;
   itemList: AngularFireList<any>;
   friendChat = firebase.database().ref('/friendchats');
 
-  constructor(public db: AngularFireDatabase) { 
-
-    this.summaries = []
-  
-  
-
-
-    
- 
-  }
+  constructor(public db: AngularFireDatabase) { }
 
 
   ngOnInit() {
-
-   
-    /// call first node in firebase summaries chat person
-    this.itemList = this.db.list('/friendchats/admin'); 
-   
+    this.itemList = this.db.list('/friendchats/admin');
     this.itemList.snapshotChanges().subscribe(actions => {
-      this.summaries = []
       actions.forEach(action => {
         this.summaries.push(action.key);
       });
-      
     });
-    
-    
   }
 
-
   onChange(select) {
-    this.selectedValue=select;
+    this.selectedValue = select;
 
     this.itemList = this.db.list('/friendchats/admin/' + this.selectedValue)
     this.itemList.snapshotChanges().subscribe(actions => {
@@ -57,41 +39,31 @@ export class ChatComponent implements OnInit {
         this.list.push(y);
         console.log(y);
       });
-  
-  
-
-
     });
-    }
-  
-    addmessage()
-    {
+  }
 
-      console.log("hello");
-      this.summaries = []
-      var promise = new Promise((resolve, reject) => {
+  addmessage() {
+    this.summaries = []
+    var promise = new Promise((resolve, reject) => {
 
-        let st=this.newmessage;
-        this.itemList = this.db.list('/friendchats/admin/'+ this.selectedValue); 
-        this.itemList. push({                        //firebase.auth().currentUser.uid
+      let st = this.newmessage;
+      this.itemList = this.db.list('/friendchats/admin/' + this.selectedValue);
+      this.itemList.push({
+        sentby: 'admin',
+        message: st,
+        timestamp: firebase.database.ServerValue.TIMESTAMP
+      }).then(() => {
+        this.itemList = this.db.list('/friendchats/' + this.selectedValue + '/admin/');
+        this.itemList.push({
           sentby: 'admin',
           message: st,
           timestamp: firebase.database.ServerValue.TIMESTAMP
         }).then(() => {
-          this.itemList = this.db.list('/friendchats/'+ this.selectedValue+'/admin/'); 
-        this.itemList. push({
-            sentby: 'admin',
-            message: st,
-            timestamp: firebase.database.ServerValue.TIMESTAMP
-          }).then(() =>{
-            resolve(true);
-          })
-              })
-          })
-          this.newmessage="";
-        return promise;
-        
-    }
-  
-
+          resolve(true);
+        })
+      })
+    })
+    this.newmessage = "";
+    return promise;
+  }
 }
